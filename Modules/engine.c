e43aa2556efef6e5cea22602c7c484fdac638cc1
Tristan COWEN <Tristancowen@hotmail.com>
2025-02-06 09:49:17 +0000

    #3
    Housekeeping, moving everything into folders

--- Modules/engine.c
+++ Modules/engine.c
@@ -12,21 +12,30 @@ Vector3D New3D(int x, int y, int z){
     return newVector;
 }
 
-SDL_Rect NewRect(Vector2D rectPos, Vector2D rectSize){
+SDL_Rect *NewRect(Vector2D rectPos, Vector2D rectSize){
     SDL_Rect newRect = {rectPos.x, rectPos.y, rectSize.x, rectSize.y};
-    return newRect;
+    return &newRect;
 }
 
 Sprite NewSprite(Vector2D spritePosition, Vector2D spriteSize, Vector2D spriteSheetPosition){
-    SDL_Rect sourceRect = NewRect(spritePosition, spriteSize);
-    SDL_Rect destRect = NewRect(spriteSheetPosition, spriteSize);
+    SDL_Rect *sourceRect = NewRect(spritePosition, spriteSize);
+    SDL_Rect *destRect = NewRect(spriteSheetPosition, spriteSize);
 
     Sprite newSprite = {sourceRect, destRect, SDL_TRUE, 1};
     return newSprite;
 }
 
-/* Drawing Functions */
-
-void Copy_Sprite_To_Buffer(Sprite spriteToDraw, SDL_Renderer *renderer){
+Map NewMap(){
     
 }
+
+/* Drawing Functions */
+void Copy_Sprite_To_Buffer(Sprite spriteToCopy, SDL_Renderer *renderer){
+    SDL_RenderCopy(renderer, tileSet, spriteToCopy.spriteSourceRect, spriteToCopy.spriteDestRect);
+    printf("Copying to buffer\n"); 
+}
+
+void Draw_Buffer(SDL_Renderer *renderer){
+    SDL_RenderPresent(renderer);
+}
+
--- main.c
+++ main.c
@@ -11,12 +11,5 @@ int main(int argc, char *argv[]) {
     mainLoop();
     cleanupSDL();
 
-    SDL_VERSION(&compiled);
-    SDL_GetVersion(&linked);
-    SDL_Log("We compiled against SDL version %u.%u.%u ...\n",
-        compiled.major, compiled.minor, compiled.patch);
-    SDL_Log("But we are linking against SDL version %u.%u.%u.\n",
-        linked.major, linked.minor, linked.patch);
-
     return 0;
 }
